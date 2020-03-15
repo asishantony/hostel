5b5adbbd2b16450562eea56348a177294b6184c8
asishantony <asishantony@gmail.com>
2020-03-15 06:23:27 +0530

    Hello world and Component creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greet from "./components/Greet";
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Greet />
+      <Welcome />
     </div>
   );
 }
